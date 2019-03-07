aab8c132d8b00784cdce922ba8c2117e8f3fcb1b
BNalini <naliniboggarapu@gmail.com>
2019-03-07 19:56:05 +0530

    UI changes

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -1,79 +1,22 @@
+/**
+ * @ngdoc function
+ * @name mobifixApp.controller:homeCtrl
+ * @description
+ * # homeCtrl
+ * Controller of the mobifixApp
+ */
+(function (angular) {
+  'use strict';
 
-(function(angular) {
-    'use strict';
-
-  function homeControllerConstructor($sce, httpDataService, commonModal, $scope, $rootScope, $http) {
-
+  function homeControllerConstructor($scope, $http, httpDataService, $state, $rootScope, commonModal) {
 
     var vm = this;
-    vm.userData = $rootScope.userData[0];
-    vm.app = 'Mobifix';
+    vm.$state = $state;
 
-    //$("#registrationForm input").attr("disabled",true);
-    $("#updateSuccess").hide();
-    $("#updateFailure").hide();
+    var vm = this;
+    vm.app = 'Mobifix'
 
-
-
-
-    function userProfile() {
-      //$("#registrationForm input").attr("disabled",false);
-      vm.userProfileCred = {
-
-        CustAdminid: vm.userData.custvendAdminID,
-        LoginId: vm.userData.LoginId,
-        FirstName: vm.userData.FirstName,
-        LastName: vm.userData.LastName,
-        FullName: "",
-        AddressLine1: vm.userData.AddressLine1,
-        AddressLine2: vm.userData.AddressLine2,
-        City: vm.userData.City,
-        State: vm.userData.State,
-        Country: vm.userData.Country,
-        ZIPCode: vm.userData.ZIPCode,
-        NoOfAttempts: "",
-        LastLoginDate: "",
-        UserStatus: "",
-        CreatedDate: "",
-        CrearedBy: 1,
-        LastUpdateDate: "",
-        LastUpdateBy: 1,
-        NamePrefix: "",
-        Gender: "",
-        AddDate: "",
-        AddedByUserID: "",
-        ChangedDate: "",
-        ChangedByID: "",
-        ContactAddrID: "",
-        ContactStatusCD: "",
-        ContactPhoneID: "",
-        CustID: "",
-        ContactNumber: "",
-        ContactStatus: "",
-        AddedDate: "",
-        AddByUserID: "",
-        ChangeDate: "",
-        ChangeByID: ""
-      }
-
-      httpDataService.userProfile(vm.userProfileCred).then(function (resposeObj) {
-        if (resposeObj.status == 200) {
-          $("#updateSuccess").show();
-          $("#registrationForm input").attr("disabled", true);
-        } else if (resposeObj.status == 404) {
-          // Error Scenarios
-          $rootScope.$broadcast("userProfilebroadcast", { status: 404 });
-          $("#updateFailure").show();
-        }
-      });
-
-
-
-    }
-    vm.userProfile = userProfile;
-    //vm.edituserProfile=edituserProfile
-    vm.userData = $rootScope.userData[0];
   }
-    angular.module('mobifixApp')
-        .controller('homeCtrl', homeControllerConstructor);
+  angular.module('mobifixApp')
+    .controller('homeCtrl', homeControllerConstructor);
 })(angular);
